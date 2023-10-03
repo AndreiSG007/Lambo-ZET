4dc79b47b43f7a7dddc825c1e2db4675d6ec4b3d
Norbert <norbert@softgenius.com>
2023-10-03 16:00:34 +0300

    Add telegram link

--- app.vue
+++ app.vue
@@ -10,8 +10,12 @@
       <div @click="casinoRedirect">
         <button class="main-button">{{ $t("playNow") }}</button>
       </div>
-      <div>
+      <div class="hero">
         <img src="/assets/header_group_Zet.png" alt="" class="main-image" />
+        <a class="hero__social" href="https://t.me/+MJpONQyVqQxlNTU0" target="blank">
+            <img src="/assets/telegram.png" alt="telegram"/>
+            <p>{{ $t('joinTelegram') }}</p>
+        </a>
       </div>
 
       <div class="steps">
