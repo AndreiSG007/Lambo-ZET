aea3b73273629d8206f2ca27ebcce2f7087465ec
Norbert <norbert@softgenius.com>
2023-10-11 00:20:10 +0300

    Revert "Add telegram link"
    This reverts commit 4dc79b47b43f7a7dddc825c1e2db4675d6ec4b3d.

--- app.vue
+++ app.vue
@@ -10,12 +10,8 @@
       <div @click="casinoRedirect">
         <button class="main-button">{{ $t("playNow") }}</button>
       </div>
-      <div class="hero">
+      <div>
         <img src="/assets/header_group_Zet.png" alt="" class="main-image" />
-        <a class="hero__social" href="https://t.me/+MJpONQyVqQxlNTU0" target="blank">
-            <img src="/assets/telegram.png" alt="telegram"/>
-            <p>{{ $t('joinTelegram') }}</p>
-        </a>
       </div>
 
       <div class="steps">
